9d393e0b4129dac2a26b8f28addd754efcc7aa97
aalahyan <me.a.alahyane@gmail.com>
2025-03-17 08:48:50 +0000

    finished expansion , wildcard left

--- expansion/expand.c
+++ expansion/expand.c
@@ -6,7 +6,7 @@
 /*   By: aalahyan <aalahyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 21:44:28 by aalahyan          #+#    #+#             */
-/*   Updated: 2025/03/17 07:19:45 by aalahyan         ###   ########.fr       */
+/*   Updated: 2025/03/17 08:48:22 by aalahyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,5 @@ char	**expand(char **arr, t_list *env_l)
 	if (!vars_expanded)
 		return (NULL);
 	wildcards_expanded = expand_wildcards(vars_expanded);
-	return (vars_expanded);
+	return (wildcards_expanded);
 }
--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: aalahyan <aalahyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 17:45:36 by aaitabde          #+#    #+#             */
-/*   Updated: 2025/03/17 07:25:42 by aalahyan         ###   ########.fr       */
+/*   Updated: 2025/03/17 08:48:15 by aalahyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,31 +36,30 @@ int main(int ac, char **av, char **env)
 	t_list	*env_l;
 
 
-	char	*e = expand_quotes("jjdd'jkh'dd");
-	// rl_catch_signals = 0;
-	// (void)ac;
-	// (void)av;
-	// env_l = build_env(env);
-	// if (!env_l)
-	// 	exit(1);
-	// draw_ascii_art();
-	// handle_signals();
+	rl_catch_signals = 0;
+	(void)ac;
+	(void)av;
+	env_l = build_env(env);
+	if (!env_l)
+		exit(1);
+	draw_ascii_art();
+	handle_signals();
 
-	// while (1)
-	// {
-	// 	prompt = readline("msh$ ");
-	// 	if (!prompt || ft_strncmp(prompt, "exit", 4) == 0)
-	// 	{
-	// 		if (!prompt)
-	// 			write(1, "exit\n", 5);
-	// 		exit(prompt == NULL);
-	// 	}
-	// 	add_history(prompt);
-	// 	ast = parse(prompt, env_l);
-	// 	process_heredocs(ast);
-	// 	execute_ast(ast, env_l);
-	// 	free_ast(ast);
-	// }
-	// clear_env(env_l);
-	// rl_clear_history();
+	while (1)
+	{
+		prompt = readline("msh$ ");
+		if (!prompt || ft_strncmp(prompt, "exit", 4) == 0)
+		{
+			if (!prompt)
+				write(1, "exit\n", 5);
+			exit(prompt == NULL);
+		}
+		add_history(prompt);
+		ast = parse(prompt, env_l);
+		process_heredocs(ast);
+		execute_ast(ast, env_l);
+		free_ast(ast);
+	}
+	clear_env(env_l);
+	rl_clear_history();
 }
