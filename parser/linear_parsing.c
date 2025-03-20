e9fe1dde1e26450d8ca9bfed4dd81e9259455908
aalahyan <me.a.alahyane@gmail.com>
2025-03-20 23:33:42 +0000

    git push

--- parser/linear_parsing.c
+++ parser/linear_parsing.c
@@ -6,7 +6,7 @@
 /*   By: aalahyan <aalahyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 22:03:29 by aalahyan          #+#    #+#             */
-/*   Updated: 2025/03/19 20:51:19 by aalahyan         ###   ########.fr       */
+/*   Updated: 2025/03/20 23:32:10 by aalahyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -150,7 +150,7 @@ bool	compare_expectations(t_l_parse *prev, t_l_parse *curr)
 	}
 	else if (prev->type == L_OR || prev->type == L_AND || prev->type == L_PIPE || prev->type == L_SEMICOLON || prev->type == L_AMPERCENT || prev->type == L_REDIRECTION)
 	{
-		if (curr->type == L_CLOSE_P || curr->type == L_OR || curr->type == L_AND || curr->type == L_PIPE || curr->type == L_SEMICOLON || curr->type == L_AMPERCENT || curr->type == L_REDIRECTION)
+		if (curr->type == L_CLOSE_P || curr->type == L_OR || curr->type == L_AND || curr->type == L_PIPE || curr->type == L_SEMICOLON || curr->type == L_AMPERCENT)
 		{
 			ft_putstr_fd("msh: syntax error near unexpected token `", 2);
 			ft_putstr_fd(curr->content, 2);
