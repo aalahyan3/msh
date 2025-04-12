67c9fed576736cd6ecb2f829f80934448b66af63
aalahyan <me.a.alahyane@gmail.com>
2025-04-12 17:45:20 +0100

    signal ctr + c

--- executor/signals_handeling.c
+++ executor/signals_handeling.c
@@ -6,18 +6,18 @@
 /*   By: aalahyan <aalahyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 11:26:25 by aaitabde          #+#    #+#             */
-/*   Updated: 2025/04/12 17:43:51 by aalahyan         ###   ########.fr       */
+/*   Updated: 2025/04/12 17:44:43 by aalahyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executor.h"
 
-void	handle_sig(int sig)
-{
-	(void)sig;
-	ft_putstr_fd("\n", 2);
-	rl_on_new_line();
-	rl_replace_line("", 0);
-	rl_redisplay();
-	g_signal_received = 1;
-}
+// void	handle_sig(int sig)
+// {
+// 	(void)sig;
+// 	ft_putstr_fd("\n", 2);
+// 	rl_on_new_line();
+// 	rl_replace_line("", 0);
+// 	rl_redisplay();
+// 	g_signal_received = 1;
+// }
