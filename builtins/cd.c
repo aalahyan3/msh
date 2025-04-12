cf08fe05890fab7aa4010c844666fa291676f711
Ayoub Aitabderrahmane <aaitabde@e3r2p10.1337.ma>
2025-04-12 17:13:48 +0100

    cd

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: aaitabde <aaitabde@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 13:43:44 by aaitabde          #+#    #+#             */
-/*   Updated: 2025/04/12 13:00:18 by aaitabde         ###   ########.fr       */
+/*   Updated: 2025/04/12 17:13:21 by aaitabde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,11 +85,12 @@ static int	cd_dash_case(t_msh *msh, char *old_path)
 int	ft_cd(char *path, t_msh *msh)
 {
 	char		*old_path;
+	char		*new_path;
 	struct stat	st;
 	int			ret;
 
 	old_path = getcwd(NULL, 0);
-	if (!path || ft_strncmp(path, "~", 2) == 0
+	if (!path || ft_strncmp(path, "~", 2) == 0 \
 		|| ft_strncmp(path, "--", 3) == 0)
 		path = get_from_env("HOME", msh->env);
 	if (!path)
@@ -97,18 +98,23 @@ int	ft_cd(char *path, t_msh *msh)
 				NULL, NULL, NULL), 1);
 	if (ft_strncmp(path, "-", 2) == 0)
 		return (cd_dash_case(msh, old_path));
-	if (ft_strncmp(path, "..", 3) == 0 || ft_strncmp(path, ".", 2) == 0)
+	ret = chdir(path);
+	if (ret != 0)
 	{
-		ret = chdir(path);
-		if (ret == 0)
-			update_pwd_env_vars(msh, old_path);
+		if (stat(path, &st) != 0 || !S_ISDIR(st.st_mode))
+			return (free(old_path), ft_printf_error(path, ": ",
+					"No such file or directory\n", NULL), 1);
 		return (free(old_path), ret);
 	}
-	if (stat(path, &st) != 0 || !S_ISDIR(st.st_mode))
-		return (free(old_path), ft_printf_error(path, ": ",
-				"No such file or directory\n", NULL), 1);
-	ret = chdir(path);
-	if (ret == 0)
-		update_pwd_env_vars(msh, old_path);
-	return (free(old_path), ret);
+	new_path = getcwd(NULL, 0);
+	if (!new_path)
+	{
+		ft_printf_error("cd: error retrieving current directory: getcwd: \
+		cannot access parent directories: No such file or directory\n", NULL, NULL, NULL);
+		free(old_path);
+		return (1);
+	}
+	update_pwd_env_vars(msh, old_path);
+	free(new_path);
+	return (free(old_path), 0);
 }
