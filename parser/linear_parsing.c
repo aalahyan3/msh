8078ef70dd3f3702e8acca2ddff392beef6795fe
aalahyan <me.a.alahyane@gmail.com>
2025-04-13 18:01:47 +0100

    pushing fixed parsing

--- parser/linear_parsing.c
+++ parser/linear_parsing.c
@@ -6,7 +6,7 @@
 /*   By: aalahyan <aalahyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 22:03:29 by aalahyan          #+#    #+#             */
-/*   Updated: 2025/04/13 16:58:46 by aalahyan         ###   ########.fr       */
+/*   Updated: 2025/04/13 18:00:12 by aalahyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,9 @@ static bool	is_unexpected_token(t_l_parse *prev, t_l_parse *curr)
 		return (curr->type == L_CLOSE_P || curr->type == L_OR \
 		|| curr->type == L_AND || curr->type == L_PIPE \
 		|| curr->type == L_SEMICOLON || curr->type == L_AMPERCENT);
+	if (prev->type == L_REDIRECTION || prev->type == L_PIPE \
+	|| prev->type == L_AND || prev->type == L_OR)
+		return (curr->type == prev->type);
 	if (prev->type == L_OPEN_P)
 		return (curr->type == L_CLOSE_P || curr->type == L_OR \
 		|| curr->type == L_AND || curr->type == L_PIPE \
@@ -50,7 +53,7 @@ static bool	is_unexpected_token(t_l_parse *prev, t_l_parse *curr)
 	|| prev->type == L_REDIRECTION)
 		return (curr->type == L_CLOSE_P || curr->type == L_OR \
 		|| curr->type == L_AND || curr->type == L_PIPE \
-		|| curr->type == L_SEMICOLON || curr->type == L_AMPERCENT || curr->type == L_REDIRECTION);
+		|| curr->type == L_SEMICOLON || curr->type == L_AMPERCENT);
 	if (prev->type == L_CLOSE_P)
 		return (curr->type == L_WORD || curr->type == L_OPEN_P);
 	if (prev->type == L_WORD)
