f90fdd2da03b8a9d3995ce9f5dac3d38001f11b5
aalahyan <me.a.alahyane@gmail.com>
2025-04-16 10:22:51 +0100

    fixed parse error

--- expansion/split_not_quotes.c
+++ expansion/split_not_quotes.c
@@ -6,7 +6,7 @@
 /*   By: aalahyan <aalahyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 03:02:29 by aalahyan          #+#    #+#             */
-/*   Updated: 2025/04/09 22:18:05 by aalahyan         ###   ########.fr       */
+/*   Updated: 2025/04/16 10:21:17 by aalahyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ static void	defernciate_command(char *str)
 	i = 0;
 	while (str[i])
 	{
-		if ((str[i] && str[i] == '\'') || str[i] == '"')
+		if (str[i] == '\'' || str[i] == '"')
 		{
 			quote = str[i];
 			i++;
@@ -32,9 +32,11 @@ static void	defernciate_command(char *str)
 					str[i] = '\x02';
 				i++;
 			}
-			continue ;
+			if (str[i] == quote)
+				i++;
 		}
-		i++;
+		else
+			i++;
 	}
 }
 
