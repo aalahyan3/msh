473a4c5e31b0c7594569c1cefc64c7c8f6a61dbe
aalahyan <me.a.alahyane@gmail.com>
2025-04-17 11:18:37 +0100

    fixed def env

--- env/build_default_env.c
+++ env/build_default_env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   build_default_env.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaitabde <aaitabde@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aalahyan <aalahyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 15:37:01 by aalahyan          #+#    #+#             */
-/*   Updated: 2025/04/13 20:16:14 by aaitabde         ###   ########.fr       */
+/*   Updated: 2025/04/17 11:16:48 by aalahyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,6 @@ t_list	*build_default_env(void)
 	ft_lstadd_back(&env, ft_lstnew(make_env_node(pwd)));
 	free(pwd);
 	ft_lstadd_back(&env, ft_lstnew(make_env_node("SHLVL=1")));
-	ft_lstadd_back(&env, ft_lstnew(make_env_node("TERM=xterm")));
 	ft_lstadd_back(&env, \
 ft_lstnew(make_env_node("PATH=/usr/gnu/bin:/usr/local/bin:/bin:/usr/bin:.")));
 	return (env);
